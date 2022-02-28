850764b82242ce7ad76aaf034325708736f31ab6
Ivan <ivanramosatc@gmail.com>
2022-02-28 09:24:52 -0500

    update vpc

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,3 @@
-# Create VPC
 # terraform aws create vpc
 terraform {
 
@@ -9,12 +8,12 @@ locals {
   subnet_id_public_2 = aws_subnet.public-subnet-2.id
   subnet_id_private  = aws_subnet.private-subnet-1.id
   key_name           = "devops"
-  private_key_path   = "~/Terraform-workspace/terraform-vpc/devops_dec.pem"
+  private_key_path   = "~/../*.pem"
 }
 
 resource "aws_key_pair" "devops" {
   key_name   = "devops"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+u0+0gECVu/X3zJBDjklErH1rV4epRZfVKcb/cS8iwRCwE9Fs2LzWJPfDL8eEj/r7c22zHv9mIPezpTeqmVnTSSN1EWsd67dc0m93AIcxDiUYtS6Z16Fx1onCwB+lcmAtu3YMi6zZpVWnIGQPMSFkAX2XFUjyjwDq4vWZ5pEAPI5JN+me/9Lidi+E4tfqA1/dJDgmL2oMCoRnpYXs2GepAW1uJDdLW6FNyQ93tvxHZeopPYrEpwXTp6CS03dWF6kFSVykHArOtYnxk+Z+JdHPrfl4Am+0p9dyJU7258JSnQ4SvQ5V3MkNLEnDhN2M9JcSkcSCMfbx3W0i/k53tV9XxXYt5VtF9AMH5bo4o+0PvADVyxnKr+0lq9lmGWYd+JN9AoBu6Qm4UvA23nzFsYmPP6XDKlQl4LfNHeEZsY2Sd8QIMzIafW+VWOxDIsmNYPnF9rDxfGkhc9fEE2BhqyzQBEEI4Wp8OgnJETSsJx2I4J75ePK/jsIU9Q7QFzQsaas= ivanj.ramos@Ivans-MacBook-Pro.local"
+  public_key = "ssh-rsa AAAA...."
 }
 
 # Create a VPC
